717bad2cf4f6d9afcb0d2feb9d42463971ec7f3a
Anthony <tonyudeagbala@gmail.com>
2022-11-10 00:00:13 +0100

    chore: create serializer file

---
+++ cella/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.auth import get_user_model
+from django_countries.serializer_fields import CountryField
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
+from .models import Product
+
+User = get_user_model()
+
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username','password', 'password2', 'nin', 'created_at']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    
+    def save(self):
+        """
+            A method overiding DRF serializer's save method
+        """
+        user = User(
+            email = self.validated_data['email'],
+            username = self.validated_data['username'],
+            nin = self.validated_data['nin'],
+            is_verified = True
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+# User Serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['product', 'quantity', 'price']
