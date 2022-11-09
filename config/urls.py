1e982bd09eff27d252ac5126945a81654402b88e
Anthony <tonyudeagbala@gmail.com>
2022-11-09 23:59:13 +0100

    chore: add app urls

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('super_cellar/', admin.site.urls),
+    path('api/v1/', include('cella.urls')),
 ]
 admin.site.site_header = "Cella Admin"
 admin.site.site_title = "Cella Admin Portal"
