087ec789409062f1a6cbe47791921dac75898837
Anthony <tonyudeagbala@gmail.com>
2022-11-10 02:29:49 +0100

    .

--- cella/manager.py
+++ cella/manager.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from django.utils.translation import gettext_lazy as _
-
+from django.db import models
 
 class CustomUserManager(BaseUserManager):
     def _create_user(self, username, email, password, **extra_fields):
