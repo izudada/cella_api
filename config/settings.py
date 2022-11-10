0acb24aa4c84a81d1c0323bbd0b6ffa8bbfda7ba
Anthony <tonyudeagbala@gmail.com>
2022-11-10 04:22:36 +0100

    Edit config

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
     "django_filters",
     "django_countries",
     "phonenumber_field",
-     'whitenoise.runserver_nostatic',
 
     'cella',
 ]
@@ -110,23 +109,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': 5432,
-        'USER': env('DATABASE_USERNAME'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DATABASE_NAME'),
+#         'HOST': env('DATABASE_HOST'),
+#         'PORT': 5432,
+#         'USER': env('DATABASE_USERNAME'),
+#         'PASSWORD': env('DATABASE_PASSWORD'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
