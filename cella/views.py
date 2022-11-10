55c512cf1dafbdeffa8d43eaf3e71af4aca1b811
Anthony <tonyudeagbala@gmail.com>
2022-11-10 14:47:33 +0100

    Fix: verify_user

--- cella/views.py
+++ cella/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from knox.models import AuthToken
 from .serializers import RegisterSerializer, UserSerializer, BrandSerializer, ProductSerializer
 from .verify import verify_id
 import json
@@ -12,12 +11,12 @@ from django.contrib.auth import get_user_model
 
 from django.contrib.auth import login
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
 from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import check_password
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Brand, Product
+from rest_framework.authtoken.models import Token
 
 
 
@@ -37,7 +36,8 @@ def verify_user(request):
     if user_data["transactionStatus"] == "SUCCESSFUL":
         result = json.dumps(user_data["response"])
         return Response(result)
-    return Response(status=status.HTTP_404_NOT_FOUND, data=user_data["description"])
+    message = {"message": user_data["description"]}
+    return Response(status=status.HTTP_404_NOT_FOUND, data=message)
 
 
 # Register API
@@ -50,33 +50,70 @@ class RegisterAPI(generics.GenericAPIView):
         user = serializer.save()
         return Response({
         "user": RegisterSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
+        "token": Token.objects.get_or_create(user=user)[0][1]
         })
 
+
+# @api_view(["POST"])
+# @permission_classes([AllowAny])
+# def login_user(request):
+
+#         data = {}
+#         body = json.loads(request.body)
+#         email = body['email']
+#         password = body['password']
+#         try:
+
+#             account = User.objects.get(email=email)
+#         except BaseException as e:
+#             raise ValidationError({"400": f'{str(e)}'})
+
+#         token = Token.objects.get_or_create(user=account)[0].digest
+#         if not check_password(password, account.password):
+#             raise ValidationError({"message": "Incorrect Login credentials"})
+
+#         if account:
+#             if account.is_active:
+#                 login(request, account)
+#                 data["message"] = "user logged in"
+#                 data["email_address"] = account.email
+
+#                 Res = {"data": data, "token": token}
+
+#                 return Response(Res)
+
+        #     else:
+        #         raise ValidationError({"400": f'Account not active'})
+
+        # else:
+        #     raise ValidationError({"400": f'Account doesnt exist'})
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def login_user(request):
 
         data = {}
-        body = json.loads(request.body)
-        email = body['email']
-        password = body['password']
+        reqBody = json.loads(request.body)
+        email1 = reqBody['Email_Address']
+        print(email1)
+        password = reqBody['password']
         try:
 
-            account = User.objects.get(email=email)
+            Account = User.objects.get(Email_Address=email1)
         except BaseException as e:
             raise ValidationError({"400": f'{str(e)}'})
 
-        token = AuthToken.objects.get_or_create(user=account)[0].digest
-        if not check_password(password, account.password):
+        token = Token.objects.get_or_create(user=Account)[0].key
+        print(token)
+        if not check_password(password, Account.password):
             raise ValidationError({"message": "Incorrect Login credentials"})
 
-        if account:
-            if account.is_active:
-                login(request, account)
+        if Account:
+            if Account.is_active:
+                print(request.user)
+                login(request, Account)
                 data["message"] = "user logged in"
-                data["email_address"] = account.email
+                data["email_address"] = Account.email
 
                 Res = {"data": data, "token": token}
 
