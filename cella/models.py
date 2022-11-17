440afe11c778e45b42b0cc12167cdd955cd5f475
Anthony <tonyudeagbala@gmail.com>
2022-11-17 10:09:53 +0100

    fix: edit email backend

--- cella/models.py
+++ cella/models.py
@@ -52,7 +52,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
 
     send_mail(
         # title:
-        "Password Reset for {title}".format(title="Some website title"),
+        "Password Reset",
         # message:
         email_plaintext_message,
         # from:
